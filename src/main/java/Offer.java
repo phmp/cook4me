ab505c95273f49719349ea0373595ed7ec4bbd3a
Paweł Proc <pawel.henryk.proc@gmail.com>
2016-11-29 23:08:38 +0100

    Offer class

---
+++ src/main/java/Offer.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Paweł on 2016-11-29.
+ */
+public class Offer {
+
+    private long id;
+    private String name;
+    private String place;
+    private String date;
+    private String description;
+    private String contact;
+    
+}
