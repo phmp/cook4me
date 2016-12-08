f810372d72781735236b33fa44f6466b061fcbfd
gaabral <drag.gabriela@gmail.com>
2016-12-08 21:39:42 +0100

    Initial commit for test class

---
+++ src/test/java/OfferManagerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Gaba on 2016-12-08.
+ */
+public class OfferManagerTest {
+
+    @Test
+    public void testStoreOffer(){
+        System.out.println("Inside testStoreOffer()");
+    }
+}
