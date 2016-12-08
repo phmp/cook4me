b3fb61074ab9d257016ff51c28805d2f7f17c63a
Adam Pelczar <adapel@op.pl>
2016-12-08 15:39:57 +0100

    Remove unsed code

--- src/main/webapp/resources/js/addOrder.js
+++ src/main/webapp/resources/js/addOrder.js
@@ -16,13 +16,6 @@ function activateItem() {
 	var params = '{"name":"' + name + '","place":"' + place + '","date":"' + date + '","contact":"' + contact + '","description":"' + description + '"}'
 	http.open("POST", url, true);
 	http.setRequestHeader("Content-type", "application/json");
-	/*
-	http.onreadystatechange = function() {//Call a function when the state changes.
-	if(http.readyState == 4 && http.status == 200) {
-		alert(http.responseText);
-	    }
-	}
-	*/
 	http.send(params);
 }
 
