e9aa666fe969a71621527c9e55987107342171a4
Pawel Proc <pawel.henryk.proc@gmail.com>
2016-12-08 17:04:27 +0100

    explicit imports

--- src/main/java/main/RestManager.java
+++ src/main/java/main/RestManager.java
@@ -3,7 +3,11 @@ package main;
 import controller.OfferManager;
 import model.Offer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.logging.Logger;
