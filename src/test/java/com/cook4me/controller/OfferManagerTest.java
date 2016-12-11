32a04276229c04b2f4a50db1829fd26b3fb43d51
gaabral <drag.gabriela@gmail.com>
2016-12-11 16:28:56 +0100

    New tests cases added

--- src/test/java/com/cook4me/controller/OfferManagerTest.java
+++ src/test/java/com/cook4me/controller/OfferManagerTest.java
@@ -1,32 +1,48 @@
 package com.cook4me.controller;
 
-import com.cook4me.controller.OfferManager;
 import com.cook4me.model.Offer;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+
 
 /**
  * Created by Gaba on 2016-12-08.
  */
+
 public class OfferManagerTest {
     private Offer offer;
+    private OfferManager offerManager;
 
     @Before
     public void setUp(){
-       offer = new Offer("bigos", "Krakow", "20.12.2016", "yummy stuff", "880880880");
-
-    }
+        offerManager = new OfferManager();
+        offer = new Offer("bigos", "Krakow", "20.12.2016", "yummy stuff", "880880880");
+        }
 
     @Test
     public void testStoreOffer(){
-        OfferManager offerManager = new OfferManager();
         offerManager.store(offer);
 
         Assert.assertEquals(1, offerManager.getOffers().size());
         Assert.assertEquals(offer, offerManager.getOffer(offer.getId()));
         Assert.assertEquals("bigos", offerManager.getOffer(offer.getId()).getName());
-
+        Assert.assertEquals(offer, offerManager.getOffers().get(0));
 
     }
+
+    @Test
+    public void negativeTest(){
+        Assert.assertNotEquals(offer, offerManager.getOffers());
+        Assert.assertNull(offerManager.getOffer((long) 2));
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullPointerException(){
+        offerManager.getOffer((long) 12).getName();
+        offerManager.getOffer((long) 34).getId();
+
+    }
+
+
+
 }
