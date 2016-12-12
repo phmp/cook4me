b6c530db401bfa2e66e51f1c033ec96989d3c6f8
gaabral <drag.gabriela@gmail.com>
2016-12-12 19:26:36 +0100

    Test for RestManager.

---
+++ src/test/java/com/cook4me/application/RestManagerTest.java
@@ -0,0 +1,63 @@
+package com.cook4me.application;
+
+import com.cook4me.controller.OfferManager;
+import com.cook4me.model.Offer;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+/**
+ * Created by Gaba on 2016-12-10.
+ */
+public class RestManagerTest {
+    @Mock
+    private OfferManager offerManager;
+
+    @InjectMocks
+    private RestManager restManager;
+
+    private Offer offer;
+
+    @Before
+    public void setUp(){
+      offerManager = mock(OfferManager.class);
+      offer = new Offer("pierogi", "Krakow", "11.04.2014", "ruskie", "888888888");
+      restManager = new RestManager();
+
+      MockitoAnnotations.initMocks(this);
+      when(offerManager.store(offer)).thenReturn(offer);
+
+    }
+
+    @Test
+    public void testStoreOffer1(){
+        Offer storedOffer = restManager.storeOffer(offer);
+        verify(offerManager, times(1)).store(offer);
+        when(offerManager.getOffers()).thenReturn(new ArrayList<Offer>(){{add(offer);}});
+        Assert.assertEquals(offer, storedOffer);
+        Assert.assertEquals("pierogi", storedOffer.getName());
+        Assert.assertEquals(1, restManager.getOffers().size());
+        Assert.assertEquals(offer, restManager.getOffers().get(0));
+
+    }
+
+    @Test
+    public void testStoreOffer2(){
+        System.out.println("no more test cases because it is all delegation");
+
+    }
+
+
+}
