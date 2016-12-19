d20dad9498c1e741bf796a4053ee1dc579d2153d
Adam <adapel@op.pl>
2016-12-19 18:35:30 +0100

    List offers prototype

--- src/main/webapp/resources/js/addOrder.js
+++ src/main/webapp/resources/js/addOrder.js
@@ -4,6 +4,8 @@ var addOrder = document.getElementById("addOrder");
 orderButton.addEventListener("click", activateItem);
 addOrder.addEventListener("click", clearText);
 
+var orders;
+
 function activateItem() {
 	var name = listItems[0].value;
 	var date = listItems[1].value;
@@ -16,6 +18,12 @@ function activateItem() {
 	var params = '{"name":"' + name + '","place":"' + place + '","date":"' + date + '","contact":"' + contact + '","description":"' + description + '"}'
 	http.open("POST", url, true);
 	http.setRequestHeader("Content-type", "application/json");
+    http.onreadystatechange = function() {//Call a function when the state changes.
+    	if(http.readyState == 4 && http.status == 200) {
+    		loadDoc();
+    	}
+    }
+
 	http.send(params);
 }
 
@@ -23,4 +31,34 @@ function clearText() {
     for (i = 0; i < listItems.length; i++) {
         listItems[i].value = "";
     }
+}
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  var url = "/offers";
+  xhttp.open("GET", url, true);
+  xhttp.onload = function() {
+    var offersList = JSON.parse(xhttp.responseText);
+    maxId = offersList.length;
+    console.log("Ilosc ofert: " + maxId);
+    lastOffer(maxId);
+   /*
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+    */
+  };
+  xhttp.send();
+}
+
+function lastOffer(index) {
+var request = new XMLHttpRequest();
+    var urlOffer = "/offers/" + index;
+    request.open("GET", urlOffer, true);
+    request.onload = function() {
+        lastAdded = JSON.parse(request.responseText);
+            console.log("Potrawa: " + lastAdded.name);
+        displayOffer(lastAdded)
+    }
+    request.send();
 }
--- src/main/webapp/resources/js/listOrders.js
+++ src/main/webapp/resources/js/listOrders.js
@@ -1,30 +1,46 @@
-function addDiv() {
-	var string = 'https://placehold.it/150x80?text=IMAGE';
+function displayAllOffers() {
+    var xhttp = new XMLHttpRequest();
+    var url = "/offers";
+    xhttp.open("GET", url, true);
+    xhttp.onload = function() {
+        var offersList = JSON.parse(xhttp.responseText);
+        console.log(offersList.length);
+        if (this.readyState == 4 && this.status == 200) {
+             for (var i = 0; i < offersList.length; i++) {
+                    displayOffer(offersList[i])
+                }
+        }
+    };
+    xhttp.send();
+}
+
+function displayOffer(offer) {
+console.log("HERE");
+    var string = 'https://placehold.it/150x80?text=IMAGE';
     var objTo = document.getElementById('main-container')
     var divtest = document.createElement("div");
     divtest.className = "col-sm-4";
-    
+    console.log("HERE2");
+
     var pannelPrimary = document.createElement("div");
 	pannelPrimary.className = "panel panel-primary";
 	divtest.appendChild(pannelPrimary);
+    console.log("HERE3");
 
 	var dishImage = document.createElement("img");
 	dishImage.src = string;
 	dishImage.style = "width:100%";
 	dishImage.className = "img-responsive";
 	pannelPrimary.appendChild(dishImage);
+    console.log("HERE4");
 
     var pannelFooter = document.createElement("div");
     pannelFooter.className = "panel-footer";
 	pannelPrimary.appendChild(pannelFooter);
+    console.log("HERE5");
 
-
-    pannelFooter.innerHTML = "new div"
-    objTo.appendChild(divtest)
+    pannelFooter.innerHTML = offer.name + "<br>" + offer.date + "<br>" + offer.place + "<br>" + offer.contact;
+    objTo.appendChild(divtest);
+    console.log("HERE6");
 }
 
-function createDivs() {
-	for (i = 0; i<3; i++) {
-		addDiv();
-	}
-}
